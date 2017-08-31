ed5af5edaa4b6abcc8f9bac3e4bdb0874c8e037b
anujmehla <anujmehla@gmail.com>
2017-08-31 09:01:32 +0530

    Now main class is used to access PersonLinked List which is object oriented rather than primitive data type

--- LinkedListAlgoritm/src/com/anuj/Main.java
+++ LinkedListAlgoritm/src/com/anuj/Main.java
@@ -3,11 +3,26 @@ package com.anuj;
 public class Main {
 
     public static void main(String[] args) {
-        IntLinkedList intLinkedList = new IntLinkedList(2);
+        Person person=new Person("Alpha","1");
+        Person person1= new Person("Beta","2");
+        PersonLinkedList personLinkedList=new PersonLinkedList(person);
+        personLinkedList.insertItem(person1);
+        personLinkedList.printList();
+
+
+
+
+
+
+       /* IntLinkedList intLinkedList = new IntLinkedList();
         intLinkedList.insertItem(5);
+        intLinkedList.insertItem(2);
         intLinkedList.insertItem(8);
 //        we are going to get output in reverse order because we are shifting elements towards right
 //        so first element we are inserting will becomes the last element in the list.
+        intLinkedList.printList();
+        System.out.println();
+        intLinkedList.sortList();
         intLinkedList.printList();
 //        output:
 //        8
@@ -15,7 +30,7 @@ public class Main {
 //        2
 
 //      now let' delete some items from the list
-        intLinkedList.delete(5);
-        intLinkedList.printList();
+        intLinkedList.delete(8);
+        intLinkedList.printList();*/
     }
 }
